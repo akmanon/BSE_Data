b28f92a8c3d96ff15832e3c60b751ec2c0369946
akmanon <kashish979@gmail.com>
2019-02-10 13:13:01 +0530

    Code Refactoring

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 const fs = require('fs');
 const axios = require('axios');
 
-// let data = `{"GetDataBetween_DatabaseResult":"Output:{"DataInputValues":[{"OpenData":[{"Open":"109.9,109.9"}],"HighData":[{"High":"134.9,134.9"}],"LowData":[{"Low":"108,108"}],"CloseData":[{"Close":"129.25,129.25"}],"VolumeData":[{"Volume":"6834278,6834278"}],"DateData":[{"Date":"08/02/2019 12:00:00 AM,08/02/2019 12:00:00 AM"}]}]}"}`;
+// Overal data
 let data;
-let myR = /(Open:.+?(?=}))/g;
+
+//Regex To extract paratmeters such as open,high, low, close and date.
+let RegOpen = /(Open:.+?(?=}))/g;
+let RegClose = /(Close:.+?(?=}))/g;
+let RegHigh = /(High:.+?(?=}))/g;
+let RegLow = /(Low:.+?(?=}))/g;
+let RegDate = /(Date:.+?(?=}))/g;
+
+//HTTP Request To obtain data
 axios.post(`http://charting.bseindia.com/charting/RestDataProvider.svc/DerivDataBetween?Exch=66&ScCode=500111&fromdate=08-02-2014-00:00:00-AM&todate=08-02-2019-00:00:00-AM`)
   .then(function (response) {
     data = JSON.stringify(response.data).replace(/\\"/g, '');
-    let dataarray = data.match(myR);
-    console.log(dataarray[0]);
-    fs.writeFile('./data/Data.json', data, 'utf-8', function (err) {
+    //Open Data
+    let OpenArray = data.match(RegOpen);
+    let Open  = OpenArray[0].replace(/Open:/g,'');
+    Open = Open.split(',').map(Number);
+    //console.log(Open);
+
+    //Close Data
+    let CloseArray = data.match(RegClose);
+    let Close  = CloseArray[0].replace(/Close:/g,'');
+    Close = Close.split(',').map(Number);
+    // console.log(Close);
+
+    //High Data
+    let HighArray = data.match(RegHigh);
+    let High  = HighArray[0].replace(/High:/g,'');
+    High = High.split(',').map(Number);
+    // console.log(High);
+
+    //Low Data
+    let LowArray = data.match(RegLow);
+    let Low  = LowArray[0].replace(/Low:/g,'');
+    Low = Low.split(',').map(Number);
+    // console.log(Low);
+
+    //Date Data
+    let DateArray = data.match(RegDate);
+    let Date  = DateArray[0].replace(/Date:/g,'');
+    Date = Date.split(',');
+    // console.log(Date);
+
+
+    //Joining All parameter in one object.
+    let StockData = {
+      Open : Open,
+      High : High,
+      Low : Low,
+      Close : Close,
+      Data : Date  
+    }
+    //File System function to write Object to new Json file.
+    fs.writeFile('./data/Data.json', JSON.stringify(StockData), 'utf-8', function (err) {
       if (err) throw err;
       console.log('Done!');
     })
